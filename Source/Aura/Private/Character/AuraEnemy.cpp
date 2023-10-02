08262aa8e5ef366c28e32c6a94d61c60a48ab449
gcbragion@gmail.com <gcbragion@gmail.com>
2023-10-02 06:59:48 -0300

    Post Process Highlight

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,24 @@
 
 #include "Character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+#include "PrimitiveComponentId.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block); // altera a collisão para custom e visiblity block
+}
+
 void AAuraEnemy::HighlightActor()
 {
-	bHighlighted = true;
+	GetMesh()->SetRenderCustomDepth(true); // mostrando a borda do mesh
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED); // definir o tamanho (250)
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	bHighlighted = false;
+	GetMesh()->SetRenderCustomDepth(false); // não mostrando a borda do mesh
+	Weapon->SetRenderCustomDepth(false);
 }
