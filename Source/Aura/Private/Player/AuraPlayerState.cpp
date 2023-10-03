ce72d3daeda5dad94c4007a4dd57684bb0d7bd4a
gcbragion@gmail.com <gcbragion@gmail.com>
2023-10-03 10:35:06 -0300

    The Player State

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Nevasca
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f; // a frequência em que o server vai tentar atualizar os clientes
+}
